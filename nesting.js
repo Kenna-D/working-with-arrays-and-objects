b65640394e376b26afdd302046ccfda5666c87ff
Makenna Garrett <kenna.dunford@gmail.com>
2021-02-25 08:41:52 -0700

    debugged an issue

--- nesting.js
+++ nesting.js
@@ -184,7 +184,7 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-function looper(numsArr){
+function looper(array){
   for(let i = 0; i < numsArr.length; i++){
     for(let j = numsArr[i].length - 1; j >= 0; j--){
       if(j % 2 !== 0){
